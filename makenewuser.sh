d08e58881f67d2467702c7d998b002b85f492820
Victor Ekpo <vic@teknixco.net>
2020-01-06 16:52:14 -0600

    makenewuser shell script, cleaned up unneeded code, added screen clear and pause command

--- makenewuser.sh
+++ makenewuser.sh
@@ -10,17 +10,22 @@ echo "Are you a SUDO user or ROOT? Type Y for Yes and N for No"
 read amiroot
 if [[ "$amiroot" == "Y" ]] || [[ "$amiroot" == "y" ]] || [[ "$amiroot" == "YES" ]] || [[ "$amiroot" == "yes" ]]
 then
-echo "Checking if you are root........"
+echo "Checking if you are root......."
+sleep 2
+printf "%0.s." {1..10000}
+echo ""
 #Check if Root or Sudo
 amiadmin=$(groups | tr " ," "\n" | grep sudo)
 #if [[ "$UID" == 0 ]] || if grep root list.txt
 if [[ "$amiadmin" == "sudo" ]] || [[ "$UID" == 0 ]]
 then
+echo "Congratulations, you are ADMIN"
 echo "Let's continue"
 else
 echo "You are not ROOT OR SUDO"
 echo "Stop Trying to hack jk"
-echo "........................"
+printf "%0.s." {1..1000}
+echo ""
 echo "If you reached this by mistake, please contact your administrator"
 exit
 fi
