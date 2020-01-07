c6cfdc315526a20291e6494da59497f51e187b4d
Victor Ekpo <vic@teknixco.net>
2020-01-07 10:12:27 -0600

    cleaned up tabulation in makenewuser.sh

--- makenewuser.sh
+++ makenewuser.sh
@@ -10,26 +10,56 @@ echo "Are you a SUDO user or ROOT? Type Y for Yes and N for No"
 read amiroot
 if [[ "$amiroot" == "Y" ]] || [[ "$amiroot" == "y" ]] || [[ "$amiroot" == "YES" ]] || [[ "$amiroot" == "yes" ]]
 then
-echo "Checking if you are root......."
-sleep 2
-printf "%0.s." {1..10000}
-echo ""
-#Check if Root or Sudo
-amiadmin=$(groups | tr " ," "\n" | grep sudo)
-#if [[ "$UID" == 0 ]] || if grep root list.txt
-if [[ "$amiadmin" == "sudo" ]] || [[ "$UID" == 0 ]]
-then
-echo "Congratulations, you are ADMIN"
-echo "Let's continue"
+	reset
+	echo "Checking if you are root......."
+	sleep 2
+	printf "%0.s." {1..10000}
+	echo ""
+	#Check if Root or Sudo
+	amiadmin=$(groups | tr " ," "\n" | grep sudo)
+	if [[ "$amiadmin" == "sudo" ]] || [[ "$UID" == 0 ]]
+	then
+		echo "Congratulations, you are ADMIN"
+		echo "Let's continue"
+		sleep 2
+		reset
+		#Prompt for username
+		echo "Please enter new username"
+		read newusername
+		echo "Great, thanks ${newusername}"
+		if [[ "$UID" == 0 ]]
+		then
+			useradd -m ${newusername}
+		else
+			sudo useradd -m ${newusername}
+		fi
+		echo "Creating your user directory now."
+		echo "Will this user be Admin? Please enter Y for Yes or N for No"
+		read newuseradmin
+		if [[ "$newuseradmin" == "Y" ]] || [[ "$newuseradmin" == "Yes" ]] || [[ "$newuseradmin" == "yes" ]] || [[ "$newuseradmin" == "y" ]]
+		then
+			echo "Great, we will add this user to sudo group."
+			if [[ "$UID" == 0 ]] 
+			then
+			usermod -a -G sudo $newusername
+			else
+			sudo usermod -a -G sudo $newusername
+			fi
+		else
+			echo "Great, this user will be added as a regular user."
+			sleep 1
+		fi
+		#Now let's change ownership of the home directory to belong to the new user
+		chown $newuser
+	else
+		echo "You are not ROOT OR SUDO"
+		echo "Stop Trying to hack jk"
+		printf "%0.s." {1..1000}
+		echo ""
+		echo "If you reached this by mistake, please contact your administrator"
+		exit
+	fi
 else
-echo "You are not ROOT OR SUDO"
-echo "Stop Trying to hack jk"
-printf "%0.s." {1..1000}
-echo ""
-echo "If you reached this by mistake, please contact your administrator"
-exit
-fi
-else
-echo "You are not a ROOT or SUDO user, please contact your administrator."
-exit
+	echo "You are not a ROOT or SUDO user, please contact your administrator."
+	exit
 fi
